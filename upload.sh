d01ec3a62be6c801160c3d98bcd124862850c5e7
Marku <matias.romeo@gmail.com>
2014-07-18 02:29:28 -0300

    use guid instead of full url | added upload.sh

---
+++ upload.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# Parametro version vacio?
+if [[ "$1" == "" ]]; then
+  echo "uso: 'upload version'"
+  exit
+fi
+
+ver=$1
+
+echo "generating version $ver ...."
+
+# Cambiamos la linea que tiene la version del app.yaml
+cat appengine/app.yaml | sed 's/^version: \(.*\)$/version: '$ver'/g' > app.yaml.tmp
+mv app.yaml.tmp appengine/app.yaml
+
+# Compilamos los templates jinja2
+/c/Python27/python.exe compile_templates.py
+
+# Upload to appengine
+appcfg.py --noauth_local_webserver --oauth2 --no_cookies update appengine/.
+
